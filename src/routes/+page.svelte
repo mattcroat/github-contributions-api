4f6f51085a2b811044a1d93f1e96b7465960c5f7
mattcroat <mattcroat.dev@gmail.com>
2023-08-29 15:01:37 +0200

    chore: Landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Github Contributions API</h1>
+<svelte:head>
+	<title>GitHub API</title>
+</svelte:head>
+
+<h1>GitHub Contributions</h1>
+
+<style>
+	:global(html, body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		display: grid;
+		place-content: center;
+		font-family: sans-serif;
+		color: hsl(220 10% 98%);
+		background-color: hsl(220 10% 10%);
+	}
+
+	h1 {
+		text-align: center;
+	}
+</style>
